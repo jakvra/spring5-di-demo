988ec4142c3603763bb3c75bcb4c41556e0d6761
Jakub Vrana <jvrana@monetplus.cz>
2018-10-24 00:26:26 +0200

    using spring boot application.properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -6,17 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties"),
-})
 public class PropertyConfig {
 
     @Autowired
@@ -41,18 +33,10 @@ public class PropertyConfig {
     @Value("${guru.jms.dburl}")
     String jmsUrl;
 
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
-
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-//        fakeDataSource.setUser(user);
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
