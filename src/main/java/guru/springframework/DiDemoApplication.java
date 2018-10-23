95b3e2c4e25b5f0359d8e81725cb0a6af02bce68
Jakub Vrana <jvrana@monetplus.cz>
2018-10-24 00:03:05 +0200

    readinf properties - PropertySourcePlaceholderConfigurer, @PropertySource

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,13 +1,10 @@
 package guru.springframework;
 
-import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
-import guru.springframework.controllers.PropertyInjectedController;
-import guru.springframework.controllers.SetterInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan(
@@ -15,14 +12,14 @@ import org.springframework.context.annotation.ComponentScan;
 //)
 public class DiDemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
-		MyController myController = (MyController) ctx.getBean("myController");
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+        MyController myController = (MyController) ctx.getBean("myController");
 
-		System.out.println(myController.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
-	}
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
+
+    }
 }
