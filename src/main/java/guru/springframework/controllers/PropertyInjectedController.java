1fb5649aa31d13025c66878bf3b3697113bc9fdf
Jakub Vrana <jvrana@monetplus.cz>
2018-10-23 11:01:39 +0200

    using qualifier

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -1,5 +1,6 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,12 @@ public class PropertyInjectedController {
 
     // use interface !!
     @Autowired
-    public GreetingServiceImpl greetingService;
+//    public GreetingServiceImpl greetingService;
+//    public GreetingService greetingService; // failed
+    public GreetingService greetingServiceImpl; // succeed - instead of qualifier using reflection by name (shortcat for qualifier)
 
     public String sayHello() {
-        return greetingService.sayGreeting();
+        return greetingServiceImpl.sayGreeting();
     }
 
 }
