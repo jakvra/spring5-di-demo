62172379fd6093c49296dcf1ed7e22bc04974311
Jakub Vrana <jvrana@monetplus.cz>
2018-10-23 10:39:18 +0200

    DI using spring DI autowiring

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -1,13 +1,18 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 // NOT TO DO !!!!
+
+@Controller
 public class PropertyInjectedController {
 
     // use interface !!
+    @Autowired
     public GreetingServiceImpl greetingService;
 
-    String sayHello() {
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 
